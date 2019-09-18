54915dcfdee5b3d3c3ab78d20664e6db017216a3
Paal Joergensen <pal.jorgensen@gmail.com>
2019-09-18 14:43:07 +0200

    HFP-2789 Add support for prevent default + bugfix

--- src/scripts/ui/keys.js
+++ src/scripts/ui/keys.js
@@ -74,10 +74,14 @@ export const isSpaceOrEnterKey = (event) => {
  */
 export const onKey = ($element, keys, callback) => {
   $element.on('keydown', (event) => {
-    for (var i = 0; i < keys.lengh; i++) {
+    for (let i = 0; i < keys.length; i++) {
       const keySetup = keys[i];
+
       if (isKey(event, [keySetup.key], {ctrl: keySetup.ctrl, shift: keySetup.shift})) {
-        return callback();
+        if (keySetup.preventDefault) {
+          event.preventDefault();
+        }
+        return callback(event);
       }
     }
   });
